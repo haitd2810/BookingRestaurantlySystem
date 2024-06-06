52848af3ab5e00ff462ff775e58627a58f6bdde0
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-06-06 11:47:04 +0700

    Add feature login screen

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace booking.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["CurrentPage"] = "Login";
+            return View();
+        }
+    }
+}
