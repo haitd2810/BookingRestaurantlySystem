7ddca33223a2ae1c3cc757998c00197e562b2ee2
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-07-04 07:39:46 +0700

    Update backend check valid user login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using booking.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace booking.Controllers
 {
     public class UserController : Controller
     {
+        private readonly bookingDBContext context = new bookingDBContext();
         public IActionResult Index()
         {
             ViewData["CurrentPage"] = "Login";
             return View();
+        }
+        private Boolean isValidStaff(string username, string password)
+        {
+            Staff staff = context.Staff.Where(staff => staff.Username == username && staff.Password == password).FirstOrDefault();
+            return staff != null;
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Staff staff)
+        {
+            if(isValidStaff(staff.Username, staff.Password))
+            {
+                return RedirectToAction("Index", "Staff");
+            }
+            else
+            {
+                return RedirectToAction("Index", "User");
+            }
         }
     }
 }
