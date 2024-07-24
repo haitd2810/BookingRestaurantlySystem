8bb2d0815951730641d18cdfc87cfb51ef0aabd9
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-07-24 20:47:48 +0700

    Implement data to order meal for table

--- Controllers/StaffController.cs
+++ Controllers/StaffController.cs
@@ -17,6 +17,16 @@ namespace booking.Controllers
 
         public ActionResult Details(int id)
         {
+            List<Meal> meal_list = context.Meals.Where(meal => meal.Status[0] == 1)
+                                   .Include(cate => cate.Cate).ToList();
+            ViewBag.mealList = meal_list;
+
+            List<Ordertable> order_list = context.Ordertables.Where(od => od.Status[0] == 1 && od.TableId == id)
+                                          .Include(od => od.Meal).ToList();
+            ViewBag.orderList = order_list;
+
+            List<Categorymeal> cate_list = context.Categorymeals.Where(cate => cate.Status[0] == 1).ToList();
+            ViewBag.category = cate_list;
             return View("Details", id);
         }
 
