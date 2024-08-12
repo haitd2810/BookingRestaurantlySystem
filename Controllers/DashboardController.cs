ee08eab3ad9d85cd643bda683b08c0bdd72b000d
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-08-12 21:17:41 +0700

    update font-end for graph

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using booking.IServices;
+using booking.Models;
+using booking.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace booking.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly Orderhistory object_odhistory = new Orderhistory();
+        private readonly Table table_object = new Table();
+        private readonly IOrderHistoryService orderHistory_service = new OrderHistoryService();
+        public IActionResult StatisticStaff()
+        {
+            List<Orderhistory> list_ord_history = object_odhistory.getAll();
+            ViewBag.order_history = list_ord_history;
+
+            List<Table> table_list = table_object.getTableList();
+            ViewBag.table_list = table_list;
+
+            List<Total_Statistics> list_total = orderHistory_service.getTotalStatistic(list_ord_history);
+            ViewBag.total = list_total;
+            return View();
+        }
+    }
+}
