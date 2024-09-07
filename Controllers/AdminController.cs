d2809b4620e2305d974c46e16c844e621fa6c2a1
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-09-07 14:23:08 +0700

    Update front-end for admin dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace booking.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+			if (ModelState.IsValid) {
+				try
+				{
+					
+				}
+				catch (Exception ex)
+				{
+					TempData["error"] = ex.Message;
+					return View("~/Views/Home/503.cshtml");
+
+				}
+				return View();
+			}
+			return View("~/Views/Home/503.cshtml");
+
+		}
+    }
+}
